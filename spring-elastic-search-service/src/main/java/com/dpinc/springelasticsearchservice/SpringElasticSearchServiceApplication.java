4c56ff0f3b7709b15cf9b6676dc7148c1bccbda0
Dhruvesh Patel <dmpatel.1412@gmail.com>
2020-08-30 23:46:23 +0100

    Added spring boot project for elastic search

---
+++ spring-elastic-search-service/src/main/java/com/dpinc/springelasticsearchservice/SpringElasticSearchServiceApplication.java
@@ -0,0 +1,13 @@
+package com.dpinc.springelasticsearchservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringElasticSearchServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringElasticSearchServiceApplication.class, args);
+	}
+
+}
